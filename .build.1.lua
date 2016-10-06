6f716e80994bfad38fd6c63767168d84005bbc91
koen visscher <koen.visscher@hotmail.com>
2016-10-06 11:37:46 +0200

    initial commit

---
+++ .build.1.lua
@@ -0,0 +1,22 @@
+
+project "SFML"
+
+    kind "StaticLib"
+ 
+    files {
+       "src/**.cpp"
+    }
+
+    includedirs {
+       "include"
+    }
+
+    # TODO ios and android!
+
+    zpm.export(function()
+     
+        includedirs { 
+            "include"
+        }
+
+    end)
