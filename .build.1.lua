9461bfada8a21e218e5a8b89db7862bc9f466dd5
koen visscher <koen.visscher@hotmail.com>
2016-10-07 19:41:06 +0200

    correct filtering

--- .build.1.lua
+++ .build.1.lua
@@ -6,18 +6,49 @@ project "SFML"
     print ( "DEBUG" );
     print ( _ACTION );   
 
+    -- generic
     files {
-       "src/**.cpp"
+       "src/SFML/Network/*.cpp",
+       "src/SFML/System/*.cpp",
+       "src/SFML/Window/*.cpp"
     }
 
-    removefiles {
-       "src/SFML/Audio/**.cpp",
-       "src/SFML/Graphics/**.cpp",
-       "src/SFML/Main/**.cpp",
-       "src/SFML/System/Android/**.cpp", 
-       "src/SFML/System/Unix/**.cpp", 
-       "src/SFML/System/Win32/**.cpp", 
-   }  
+    filter "system:windows"
+     	
+       files {
+       "src/SFML/Network/Win32/*.cpp",
+       "src/SFML/System/Win32/*.cpp",
+       "src/SFML/Window/Win32/*.cpp"
+    	}
+    
+    filter "system:linux"
+    
+       files {
+       "src/SFML/Network/Unix/*.cpp",
+       "src/SFML/System/Unix/*.cpp",
+       "src/SFML/Window/Unix/*.cpp"
+        }
+    
+    filter "system:macosx"
+    
+       files {
+       "src/SFML/Network/Unix/*.cpp",
+       "src/SFML/System/Unix/*.cpp",
+       "src/SFML/Window/OSX/*.cpp"
+        } 
+    
+    filter "action:android"
+ 
+       files {
+       "src/SFML/System/Android/*.cpp",
+       "src/SFML/Window/Android/*.cpp"	    
+       }
+
+
+
+    filter {}
+    	
+
  
     includedirs {
        "include",
