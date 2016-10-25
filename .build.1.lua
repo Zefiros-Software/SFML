5e9f5b004cfeaa6869c0f9f00654c9e876695709
PaulVisscher <paul.pev@hotmail.com>
2016-10-25 15:08:45 +0200

    Improved build file

--- .build.1.lua
+++ .build.1.lua
@@ -1,103 +1,75 @@
 project "SFML-system"
 
-    kind "StaticLib"
- 
-    defines { "SFML_STATIC" }
+    kind "StaticLib" 
 
-    includedirs {
-       "include",
-       "src"
-    }
-
-    -- generic
     files {
        "include/SFML/System/*.inl",
        "src/SFML/System/*.cpp"
     }
 
     filter "system:windows"
-
-       files 
-       {
+       files {
            "src/SFML/System/Win32/*.cpp"
        }
 
     filter "system:linux"
-
-       files 
-       {
+       files {
            "src/SFML/System/Unix/*.cpp"
        }
 
     filter "system:macosx"
-
-       files 
-       {
+       files {
            "src/SFML/System/Unix/*.cpp"
        }
     
     filter "action:android"
-
-       files 
-       {
+       files {
            "src/SFML/System/Android/*.cpp",
        }
 
     filter {} 
 
     zpm.export(function()
-
-        filter "system:macosx"
-
-            linkoptions { "-ObjC -framework CoreFoundation -framework AppKit -framework IOKit -framework Carbon -framework OpenGL" }
-
-        filter {}
+     
+        defines "SFML_STATIC"
 
         includedirs {
             "include"
         }
-     
-        defines { "SFML_STATIC" }
+
+        filter "system:macosx"
+            linkoptions "-ObjC -framework CoreFoundation -framework AppKit -framework IOKit -framework Carbon -framework OpenGL"
+
+        filter {}
  
     end)
 
 project "SFML-window"
 
-    kind "StaticLib"
- 
-    defines { "SFML_STATIC" }
+    kind "StaticLib" 
 
-    includedirs {
-       "include",
-       "src"
-    }
+    includedirs "src"
 
-
-    -- generic
     files {
        "include/SFML/Window/*.inl",
        "src/SFML/Window/*.cpp"
     }
    
-    links { "SFML-system" }
+    links "SFML-system"
 
     filter "system:windows"
-
-       files 
-       {
+       files {
            "src/SFML/Window/Win32/*.cpp"
        }
 
-       removefiles 
-       {
+       removefiles {
             "src/SFML/Window/EGLCheck.cpp",
             "src/SFML/Window/EglContext.cpp"
        }
   
-    filter "system:linux"
-   
+    filter "system:linux"   
        files {
-       "src/SFML/Window/Unix/*.cpp"
+            "src/SFML/Window/Unix/*.cpp"
         }
 
         removefiles {
@@ -107,29 +79,32 @@ project "SFML-window"
     
      filter "system:macosx"
 
-	files
- 	{
+        files {
             "src/SFML/Window/OSX/*.cpp",
             "src/SFML/Window/OSX/*.mm",
-	    "src/SFML/Window/OSX/*.m"
+	        "src/SFML/Window/OSX/*.m"
         }
-    
-	removefiles {
-            "src/SFML/Window/EGLCheck.cpp",
-            "src/SFML/Window/EglContext.cpp"
-        }
+        
+        removefiles {
+                "src/SFML/Window/EGLCheck.cpp",
+                "src/SFML/Window/EglContext.cpp"
+            }
 
      filter "action:android"
  
-        files 
-        {
+        files {
            "src/SFML/Window/Android/*.cpp"
         }
      
-     filter{}
-
+     filter {}
     
      zpm.export(function()
+
+        defines "SFML_STATIC"
+ 
+        includedirs {
+            "include"
+        }
 
         filter "system:macosx"
 
@@ -137,17 +112,12 @@ project "SFML-window"
 
         filter "system:windows"
 		
-	    links { "OpenGL32", "Winmm" }
+	        links { 
+                "OpenGL32", 
+                "Winmm" 
+            }
 
-        filter {}
-
-  
- 
-        includedirs {
-            "include"
-        }
-
-        defines { "SFML_STATIC" }
+        filter {}  
 
     end)
 
