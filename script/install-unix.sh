878495591ba9caf59bf58a4cab61fe591e2bb877
PaulVisscher <paul.pev@hotmail.com>
2017-01-22 17:07:36 +0100

    Added dependencies

--- script/install-unix.sh
+++ script/install-unix.sh
@@ -8,4 +8,13 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo update-alternatives --config gcc
     sudo update-alternatives --config g++
     sudo apt-get install gcc-${VERSION}-multilib g++-${VERSION}-multilib
+
+    # needed for SFML
+    sudo apt-get install freeglut3-dev 
+    sudo apt-get install libjpeg-dev 
+    sudo apt-get install libfreetype6-dev 
+    sudo apt-get install libxrandr-dev 
+    sudo apt-get install libglew-dev 
+    sudo apt-get install libsndfile1-dev 
+    sudo apt-get install libopenal-dev
 fi
